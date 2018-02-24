fdd9bb785290fb136efd194f601e6f488c084692
XIAOWEIKUN <weikun.xiao.2015@sis.smu.edu.sg>
2018-02-24 11:55:12 +0800

    Updated sql file
    1. updated the92shop.sql to change the data types of custId and skuId
    2. added in primary keys and foreign keys for customer_purchase table

--- the92Shop/sql/the92shop.sql
+++ the92Shop/sql/the92shop.sql
@@ -27,32 +27,32 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `customer` (
+  `Cust_Id` int(30) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
-  `Gender` varchar(10) NOT NULL,
   `Phone_number` varchar(10) NOT NULL,
   `Address` varchar(100) NOT NULL,
   `Postal_Code` varchar(30) NOT NULL,
-  PRIMARY KEY (`Phone_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`Cust_Id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`Name`, `Gender`, `Phone_number`, `Address`, `Postal_Code`) VALUES
-('Tom', 'M', '89093271', 'Bishan St Blk 7 #14-232', '123222'),
-('Alexandra', 'F', '89093272', 'Tampines St Blk 12 #14-00', '123425'),
-('Clara', 'F', '85476531', 'Bishan St Blk 2 #13-323', '123123'),
-('Amelia Tan', 'F', '85876531', '28 Jalan Datoh #26-07', '329536'),
-('Monste Lee', 'M', '97478731', 'Balestier Rd Blk 89 #13-323', '234871'),
-('Esmerinda Chong', 'F', '87776532', 'Ang Mo Kio St Blk 15 #17-30', '546123'),
-('Simpson Ang', 'M', '85400431', '13 Jalan Bahagia #01-02', '333123'),
-('Brendon Lee', 'M', '98706531', 'Tampines St Blk 45 #10-123', '324581'),
-('Ciara Seet', 'F', '93386198', 'Bishan St Blk 2 #13-323', '320913'),
-('Michael Kans', 'M', '93386189', '700 Toa Payoh Lor 1 #11-02', '329773'),
-('Michelle Lim', 'F', '93456189', '60 Toa Payoh Blk 3 #12-90', '329774'),
-('Alex Goh', 'M', '87386189', 'Clementi Avenue 5 Blk 103 #103-70', '329773'),
-('Havana Ohnana', 'F', '91807177', '67 Lorong Chuan #26-02', '329743');
+INSERT INTO `customer` (`Name`, `Phone_number`, `Address`, `Postal_Code`) VALUES
+('Tom', '89093271', 'Bishan St Blk 7 #14-232', '123222'),
+('Alexandra', '89093272', 'Tampines St Blk 12 #14-00', '123425'),
+('Clara', '85476531', 'Bishan St Blk 2 #13-323', '123123'),
+('Amelia Tan', '85876531', '28 Jalan Datoh #26-07', '329536'),
+('Monste Lee', '97478731', 'Balestier Rd Blk 89 #13-323', '234871'),
+('Esmerinda Chong', '87776532', 'Ang Mo Kio St Blk 15 #17-30', '546123'),
+('Simpson Ang', '85400431', '13 Jalan Bahagia #01-02', '333123'),
+('Brendon Lee', '98706531', 'Tampines St Blk 45 #10-123', '324581'),
+('Ciara Seet', '93386198', 'Bishan St Blk 2 #13-323', '320913'),
+('Michael Kans', '93386189', '700 Toa Payoh Lor 1 #11-02', '329773'),
+('Michelle Lim', '93456189', '60 Toa Payoh Blk 3 #12-90', '329774'),
+('Alex Goh', '87386189', 'Clementi Avenue 5 Blk 103 #103-70', '329773'),
+('Havana Ohnana', '91807177', '67 Lorong Chuan #26-02', '329743');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,7 @@ INSERT INTO `customer` (`Name`, `Gender`, `Phone_number`, `Address`, `Postal_Cod
 --
 
 CREATE TABLE IF NOT EXISTS `inventory` (
-  `SKU_Id` int(30) NOT NULL,
+  `SKU_Id` int(30) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
   `Quantity` int(11) NOT NULL,
   `Updated_Date` varchar(50) NOT NULL,
@@ -74,19 +74,19 @@ CREATE TABLE IF NOT EXISTS `inventory` (
 -- Dumping data for table `inventory`
 --
 
-INSERT INTO `inventory` (`SKU_Id`, `Name`, `Quantity`, `Updated_Date`, `Cost_Price`, `Selling_Price`) VALUES
-(1, 'The Shell (Blue)', 1, '2017-12-28', '29.90', '31.90'),
-(2, 'Plain green socks', 412, '2017-07-11', '5.00', '7.60'),
-(3, 'Plain black socks', 308, '2017-07-11', '5.00', '7.60'),
-(4, 'Plain white socks', 205, '2017-07-12', '5.50', '7.90'),
-(5, 'Plain grey socks', 200, '2017-07-11', '7.50', '9.50'),
-(6, 'Little Animal Ankle B', 8, '2017-07-11', '7.50', '9.50'),
-(7, 'Candie Socks', 14, '2017-07-13', '5.00', '7.60'),
-(8, 'Pig Socks', 4, '2017-01-03', '2.40', '4.90'),
-(9, 'White T-shirt with red polka dots', 31, '2017-07-15', '10.50', '14.50'),
-(10, 'Plain purple thick socks', 200, '2017-07-11', '7.50', '9.50'),
-(11, 'Two-toned red thick socks', 310, '2017-07-12', '7.50', '9.50'),
-(12, 'White grey striped T-shirt', 31, '2017-07-16', '11.50', '14.00');
+INSERT INTO `inventory` (`Name`, `Quantity`, `Updated_Date`, `Cost_Price`, `Selling_Price`) VALUES
+('The Shell (Blue)', 1, '2017-12-28', '29.90', '31.90'),
+('Plain green socks', 412, '2017-07-11', '5.00', '7.60'),
+('Plain black socks', 308, '2017-07-11', '5.00', '7.60'),
+('Plain white socks', 205, '2017-07-12', '5.50', '7.90'),
+('Plain grey socks', 200, '2017-07-11', '7.50', '9.50'),
+('Little Animal Ankle B', 8, '2017-07-11', '7.50', '9.50'),
+('Candie Socks', 14, '2017-07-13', '5.00', '7.60'),
+('Pig Socks', 4, '2017-01-03', '2.40', '4.90'),
+('White T-shirt with red polka dots', 31, '2017-07-15', '10.50', '14.50'),
+('Plain purple thick socks', 200, '2017-07-11', '7.50', '9.50'),
+('Two-toned red thick socks', 310, '2017-07-12', '7.50', '9.50'),
+('White grey striped T-shirt', 31, '2017-07-16', '11.50', '14.00');
 
 -- --------------------------------------------------------
 --
@@ -94,12 +94,14 @@ INSERT INTO `inventory` (`SKU_Id`, `Name`, `Quantity`, `Updated_Date`, `Cost_Pri
 --
 
 CREATE TABLE IF NOT EXISTS `customer_purchase` (
+  `Order_Id` int(30) NOT NULL AUTO_INCREMENT,
+  `Cust_Id` int(30) NOT NULL,
   `Phone_number` varchar(10) NOT NULL,
   `SKU_Id` int(30) NOT NULL,
   `Date_Of_Purchase` date NOT NULL,
   `Quantity` int(11) NOT NULL,
-  PRIMARY KEY (`Phone_number`,`SKU_Id`,`Date_Of_Purchase`),
-  FOREIGN KEY (`Phone_number`) REFERENCES `customer`(`Phone_number`),
+  PRIMARY KEY (`Order_Id`),
+  FOREIGN KEY (`Cust_Id`) REFERENCES `customer`(`Cust_Id`),
   FOREIGN KEY (`SKU_Id`) REFERENCES `inventory`(`SKU_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -107,23 +109,23 @@ CREATE TABLE IF NOT EXISTS `customer_purchase` (
 -- Dumping data for table `customer_purchase`
 --
 
-INSERT INTO `customer_purchase` (`Phone_number`, `SKU_Id`, `Date_Of_Purchase`, `Quantity`) VALUES
-('89093271', '1', '2016-01-18', 10),
-('89093272', '1', '2016-03-18', 20),
-('85476531', '2', '2017-06-03', 11),
-('85476531', '3', '2017-08-03', 15),
-('85876531', '4', '2017-07-03', 7),
-('85876531', '2', '2016-02-18', 11),
-('85876531', '5', '2017-07-11', 12),
-('97478731', '1', '2017-04-18', 16),
-('97478731', '2', '2017-04-18', 15),
-('97478731', '7', '2017-02-18', 9),
-('87776532', '4', '2016-05-01', 15),
-('85400431', '5', '2016-06-18', 20),
-('85400431', '10', '2017-01-11', 12),
-('85400431', '8', '2016-04-15', 13),
-('91807177', '2', '2017-02-13', 8),
-('91807177', '9', '2016-07-10', 6);
+INSERT INTO `customer_purchase` (`Cust_Id`,`Phone_number`, `SKU_Id`, `Date_Of_Purchase`, `Quantity`) VALUES
+('1','89093271', '1', '2016-01-18', 10),
+('2','89093272', '1', '2016-03-18', 20),
+('3','85476531', '2', '2017-06-03', 11),
+('3','85476531', '3', '2017-08-03', 15),
+('4','85876531', '4', '2017-07-03', 7),
+('4','85876531', '2', '2016-02-18', 11),
+('4','85876531', '5', '2017-07-11', 12),
+('5','97478731', '1', '2017-04-18', 16),
+('5','97478731', '2', '2017-04-18', 15),
+('5','97478731', '7', '2017-02-18', 9),
+('6','87776532', '4', '2016-05-01', 15),
+('7','85400431', '5', '2016-06-18', 20),
+('7','85400431', '10', '2017-01-11', 12),
+('7','85400431', '8', '2016-04-15', 13),
+('13','91807177', '2', '2017-02-13', 8),
+('13','91807177', '9', '2016-07-10', 6);
 
 -- --------------------------------------------------------
 
